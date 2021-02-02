255a52bea60c081b2790de4fe708fca0001cac54
Sruthi Narapareddy <sruthi1014@gmail.com>
2021-02-02 13:53:14 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,request
+import pickle
+import pandas as pd
+app= Flask(__name__)
+
+@app.route('/')
+def intro():
+    return render_template('html_harry.html')
+
+@app.route('/submit',methods=['POST'])
+def fetch():
+    csvdata=pickle.load(open('harry.pkl','rb'))
+    input=request.form['a']
+    if input == 'other':
+        input=request.form['b']
+    print(input)
+    details=csvdata.loc[[input]]
+    print(details.House)
+
+    return render_template('after.html',data=details)
+if __name__== "__main__":
+    app.run(debug=True)
